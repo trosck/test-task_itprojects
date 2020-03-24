43e792fb891001f541cdaa0c8a99fe12382eae7a
trosck <kort.dll@gmail.com>
2020-03-24 19:28:54 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ const styles = readFileSync(
 
 module.exports = {
   publicPath: process.env.NODE_ENV === "production"
-    ? "/itprojects/"
+    ? "/itprojects/docs/"
     : "/",
   outputDir: "docs",
   css: {
